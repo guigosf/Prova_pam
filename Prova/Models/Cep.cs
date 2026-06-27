729ca02bf253e3a835ff185e2f99107b2fe32864
guigosf <guisf2007@gmail.com>
2026-06-26 22:25:20 -0300

    second commit:

--- Prova/Models/Cep.cs
+++ Prova/Models/Cep.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace Prova.Models
 {
-    internal class Cep
+    public class Cep
     {
+        public int cep { get; set; }
+        public int state{ get; set; }
+        public int city { get; set; }
+        public int neighborhood { get; set; }
+        public int street { get; set; }
+
     }
 }
--- Prova/Service/CepService.cs
+++ Prova/Service/CepService.cs
@@ -1,10 +1,41 @@
-﻿using System;
+﻿using Prova.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 
 namespace Prova.Service
 {
-    internal class CepService
+    public class CepService
     {
+        HttpClient client;
+        Cep cep;
+        JsonSerializerOptions options;
+
+    public CepService()
+        {
+            client = new HttpClient();
+            options = new JsonSerializerOptions
+            {
+               PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+               WriteIndented = true
+            };
+        }
+
+        public async Task<Cep> GetCep()
+        {
+            string url = "https://brasilapi.com.br/api/cep/v1/{cep}";
+
+            HttpResponseMessage response = await this.client.GetAsync(url);
+
+            if(response.IsSuccessStatusCode)
+            {
+                var JsonResponse = await response.Content.ReadAsStringAsync();
+                cep =
+                       JsonSerializer.Deserialize<Cep>(JsonResponse, options);
+                return cep;
+            }
+            return new Cep();
+        }
     }
 }
